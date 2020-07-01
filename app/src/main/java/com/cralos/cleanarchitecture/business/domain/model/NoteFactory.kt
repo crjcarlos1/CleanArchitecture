023d10ca903051e10301870623c6f15c12eb1e5d
crjcarlos1 <crjcarlos1@users.noreply.github.com>
2020-07-01 17:35:49 -0500

    Added domain layer

---
+++ app/src/main/java/com/cralos/cleanarchitecture/business/domain/model/NoteFactory.kt
@@ -0,0 +1,40 @@
+package com.cralos.cleanarchitecture.business.domain.model
+
+import com.cralos.cleanarchitecture.business.domain.util.DateUtil
+import java.util.*
+import javax.inject.Inject
+import javax.inject.Singleton
+import kotlin.collections.ArrayList
+
+/**Clase que produce notas*/
+
+@Singleton
+class NoteFactory
+@Inject
+constructor(private val dateUtil: DateUtil) {
+
+    fun createSingleNote(id: String? = null, title: String, body: String? = null): Note {
+        return Note(
+            id = id ?: UUID.randomUUID().toString(),
+            title = title,
+            body = body ?: "",
+            created_at = dateUtil.getCurrentTimestamp(),
+            updated_at = dateUtil.getCurrentTimestamp()
+        )
+    }
+
+    fun createNoteList(numNotes: Int): List<Note> {
+        val list: ArrayList<Note> = ArrayList()
+        for (i in 0 until numNotes) {
+            list.add(
+                createSingleNote(
+                    id = null,
+                    title = UUID.randomUUID().toString(),
+                    body = UUID.randomUUID().toString()
+                )
+            )
+        }
+        return list
+    }
+
+}
