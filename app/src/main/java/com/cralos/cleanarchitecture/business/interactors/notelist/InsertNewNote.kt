5cac93e635e683dbf2b688d9d8fbc1ecef82f182
crjcarlos1 <crjcarlos1@users.noreply.github.com>
2020-07-02 13:23:43 -0500

    Added cache and network error

--- app/src/main/java/com/cralos/cleanarchitecture/business/interactors/notelist/InsertNewNote.kt
+++ app/src/main/java/com/cralos/cleanarchitecture/business/interactors/notelist/InsertNewNote.kt
@@ -1,11 +1,15 @@
 package com.cralos.cleanarchitecture.business.interactors.notelist
 
+import com.cralos.cleanarchitecture.business.data.cache.CacheResponseHandler
 import com.cralos.cleanarchitecture.business.data.cache.abstraction.NoteCacheDataSource
 import com.cralos.cleanarchitecture.business.data.network.abstraction.NoteNetworkDataSource
 import com.cralos.cleanarchitecture.business.domain.model.Note
 import com.cralos.cleanarchitecture.business.domain.model.NoteFactory
 import com.cralos.cleanarchitecture.business.domain.state.*
+import com.cralos.cleanarchitecture.business.util.safeApiCall
+import com.cralos.cleanarchitecture.business.util.safeCacheCall
 import com.cralos.cleanarchitecture.framework.presentation.notelist.state.NoteListViewState
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import java.util.*
@@ -14,7 +18,7 @@ class InsertNewNote(
     private val noteCacheDataSource: NoteCacheDataSource,
     private val noteNetworkDataSource: NoteNetworkDataSource,
     private val noteFactory: NoteFactory
-){
+) {
 
     fun insertNewNote(
         id: String? = null,
@@ -27,49 +31,60 @@ class InsertNewNote(
             title = title,
             body = ""
         )
-        val cacheResult = noteCacheDataSource.insertNote(newNote)
 
-        var cacheResponse: DataState<NoteListViewState>? = null
-        if(cacheResult > 0){
-            val viewState =
-                NoteListViewState(
-                    newNote = newNote
-                )
-            cacheResponse = DataState.data(
-                response = Response(
-                    message = INSERT_NOTE_SUCCESS,
-                    uiComponentType = UIComponentType.Toast(),
-                    messageType = MessageType.Success()
-                ),
-                data = viewState,
-                stateEvent = stateEvent
-            )
+        /**INSERT DE LA NOTA NUEVA*/
+        val cacheResult = safeCacheCall(Dispatchers.IO) {
+            noteCacheDataSource.insertNote(newNote)
         }
-        else{
-            cacheResponse = DataState.data(
-                response = Response(
-                    message = INSERT_NOTE_FAILED,
-                    uiComponentType = UIComponentType.Toast(),
-                    messageType = MessageType.Error()
-                ),
-                data = null,
-                stateEvent = stateEvent
-            )
-        }
+
+        val cacheResponse = object : CacheResponseHandler<NoteListViewState, Long>(
+            response = cacheResult,
+            stateEvent = stateEvent
+        ) {
+            override fun handleSuccess(resultObject: Long): DataState<NoteListViewState> {
+                return if (resultObject > 0) {
+                    val viewState =
+                        NoteListViewState(
+                            newNote = newNote
+                        )
+                    DataState.data(
+                        response = Response(
+                            message = INSERT_NOTE_SUCCESS,
+                            uiComponentType = UIComponentType.Toast(),
+                            messageType = MessageType.Success()
+                        ),
+                        data = viewState,
+                        stateEvent = stateEvent
+                    )
+                } else {
+                    DataState.data(
+                        response = Response(
+                            message = INSERT_NOTE_FAILED,
+                            uiComponentType = UIComponentType.Toast(),
+                            messageType = MessageType.Error()
+                        ),
+                        data = null,
+                        stateEvent = stateEvent
+                    )
+                }
+            }
+
+        }.getResult()
 
         emit(cacheResponse)
 
         updateNetwork(cacheResponse.stateMessage?.response?.message, newNote)
     }
 
-    private suspend fun updateNetwork(cacheResponse: String?, newNote: Note){
-        if(cacheResponse.equals(INSERT_NOTE_SUCCESS)){
-
-            noteNetworkDataSource.insertOrUpdateNote(newNote)
+    private suspend fun updateNetwork(cacheResponse: String?, newNote: Note) {
+        if (cacheResponse.equals(INSERT_NOTE_SUCCESS)) {
+            safeApiCall(Dispatchers.IO) {
+                noteNetworkDataSource.insertOrUpdateNote(newNote)
+            }
         }
     }
 
-    companion object{
+    companion object {
         val INSERT_NOTE_SUCCESS = "Successfully inserted new note."
         val INSERT_NOTE_FAILED = "Failed to insert new note."
     }
---
+++ app/src/main/java/com/cralos/cleanarchitecture/business/util/SingletonErrors.kt
@@ -0,0 +1,8 @@
+package com.cralos.cleanarchitecture.business.util
+
+object SingletonErrors {
+
+    const val ERROR_UNKNOWN = "Unknown error"
+    const val INVALID_STATED_EVENT = "Invalid state event"
+
+}
