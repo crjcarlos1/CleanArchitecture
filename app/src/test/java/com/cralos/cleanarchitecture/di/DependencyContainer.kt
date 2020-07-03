90c94fcc5f4d685057417e9c67b5ad23d3d27453
crjcarlos1 <crjcarlos1@users.noreply.github.com>
2020-07-02 23:34:42 -0500

    Added fakeDataSets

--- app/src/test/java/com/cralos/cleanarchitecture/di/DependencyContainer.kt
+++ app/src/test/java/com/cralos/cleanarchitecture/di/DependencyContainer.kt
@@ -1,9 +1,11 @@
 package com.cralos.cleanarchitecture.di
 
+import com.cralos.cleanarchitecture.business.data.NoteDataFactory
 import com.cralos.cleanarchitecture.business.data.cache.FakeNoteCacheDataSourceImpl
 import com.cralos.cleanarchitecture.business.data.cache.abstraction.NoteCacheDataSource
 import com.cralos.cleanarchitecture.business.data.network.FakeNoteNetworkDataSourceImpl
 import com.cralos.cleanarchitecture.business.data.network.abstraction.NoteNetworkDataSource
+import com.cralos.cleanarchitecture.business.domain.model.Note
 import com.cralos.cleanarchitecture.business.domain.model.NoteFactory
 import com.cralos.cleanarchitecture.business.domain.util.DateUtil
 import com.cralos.cleanarchitecture.util.isUnitTest
@@ -18,19 +20,28 @@ class DependencyContainer {
     lateinit var noteNetworkDataSource: NoteNetworkDataSource
     lateinit var noteCacheDataSource: NoteCacheDataSource
     lateinit var noteFactory: NoteFactory
+    lateinit var noteDataFactory: NoteDataFactory
+    private var notesData: HashMap<String, Note> = HashMap()
 
     init {
         isUnitTest = true // for Logger.kt
     }
 
     fun build() {
+        this.javaClass.classLoader?.let { classLoader ->
+            noteDataFactory = NoteDataFactory(classLoader)
+
+            //fake data set
+            notesData =
+                noteDataFactory.produceHashMapOfNotes(noteDataFactory.produceListOfNotes())
+        }
         noteFactory = NoteFactory(dateUtil)
         noteNetworkDataSource = FakeNoteNetworkDataSourceImpl(
-            notesData = HashMap(),
+            notesData = notesData,
             deletedNotesData = HashMap()
         )
         noteCacheDataSource = FakeNoteCacheDataSourceImpl(
-            notesData = HashMap(),
+            notesData = notesData,
             dateUtil = dateUtil
         )
     }
