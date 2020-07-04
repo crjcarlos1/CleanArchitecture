f4beb20273d208d26de27344d1c2947c9b5409bb
crjcarlos1 <crjcarlos1@users.noreply.github.com>
2020-07-03 19:51:47 -0500

    Added restore-delete-note-use-case

--- app/src/main/java/com/cralos/cleanarchitecture/business/interactors/notelist/RestoreDeletedNote.kt
+++ app/src/main/java/com/cralos/cleanarchitecture/business/interactors/notelist/RestoreDeletedNote.kt
@@ -1,3 +1,88 @@
 package com.cralos.cleanarchitecture.business.interactors.notelist
 
-class RestoreDeletedNote {}
+import com.cralos.cleanarchitecture.business.data.cache.CacheResponseHandler
+import com.cralos.cleanarchitecture.business.data.cache.abstraction.NoteCacheDataSource
+import com.cralos.cleanarchitecture.business.data.network.abstraction.NoteNetworkDataSource
+import com.cralos.cleanarchitecture.business.domain.model.Note
+import com.cralos.cleanarchitecture.business.domain.state.*
+import com.cralos.cleanarchitecture.business.util.safeApiCall
+import com.cralos.cleanarchitecture.business.util.safeCacheCall
+import com.cralos.cleanarchitecture.framework.presentation.notelist.state.NoteListViewState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class RestoreDeletedNote(
+    private val noteCacheDataSource: NoteCacheDataSource,
+    private val noteNetworkDataSource: NoteNetworkDataSource
+) {
+
+    fun restoreDeletedNote(
+        note: Note,
+        stateEvent: StateEvent
+    ): Flow<DataState<NoteListViewState>?> = flow {
+
+        val cacheResult = safeCacheCall(Dispatchers.IO) {
+            noteCacheDataSource.insertNote(note)
+        }
+
+        val response = object: CacheResponseHandler<NoteListViewState, Long>(
+            response = cacheResult,
+            stateEvent = stateEvent
+        ){
+            override suspend fun handleSuccess(resultObj: Long): DataState<NoteListViewState>? {
+                return if(resultObj > 0){
+                    val viewState =
+                        NoteListViewState(
+                            notePendingDelete = NoteListViewState.NotePendingDelete(
+                                note = note
+                            )
+                        )
+                    DataState.data(
+                        response = Response(
+                            message = RESTORE_NOTE_SUCCESS,
+                            uiComponentType = UIComponentType.Toast(),
+                            messageType = MessageType.Success()
+                        ),
+                        data = viewState,
+                        stateEvent = stateEvent
+                    )
+                }
+                else{
+                    DataState.data(
+                        response = Response(
+                            message = RESTORED_NOTE_FAIL,
+                            uiComponentType = UIComponentType.Toast(),
+                            messageType = MessageType.Error()
+                        ),
+                        data = null,
+                        stateEvent = stateEvent
+                    )
+                }
+            }
+        }.getResult()
+
+        emit(response)
+        updateNewtwork(response?.stateMessage?.response?.message,note)
+    }
+
+    private suspend fun updateNewtwork(response: String?, note: Note) {
+        if (response.equals(RESTORE_NOTE_SUCCESS)){
+            //Insert into 'notes' node
+            safeApiCall(Dispatchers.IO){
+                noteNetworkDataSource.insertOrUpdateNote(note)
+            }
+
+            //remove from 'deletes' node
+            safeApiCall(Dispatchers.IO){
+                noteNetworkDataSource.deleteDeletedNote(note)
+            }
+        }
+    }
+
+    companion object {
+        val RESTORE_NOTE_SUCCESS = "Successfully restored the delete note"
+        val RESTORED_NOTE_FAIL = "Failed to restore the delete note"
+    }
+
+}
